7e977a179854e999e7aa1b147819d13ca673a6b9
datkachenko <datkachenko@rtech.ru>
2024-03-17 22:25:28 +0400

    Initial commit

---
+++ src/main/java/ru/greatstep/serverapp/ServerAppApplication.java
@@ -0,0 +1,22 @@
+package ru.greatstep.serverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAppApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String test() throws InterruptedException {
+        Thread.sleep(1000);
+        return "ok";
+    }
+
+}
